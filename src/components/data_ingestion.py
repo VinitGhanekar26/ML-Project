2adb87b658d48da44de521d16a4514b778c686d8
VinitGhanekar26 <vinitghanekar281@gmail.com>
2026-06-07 13:33:26 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -25,6 +26,7 @@ class DataIngestion:
 
         try:
             df = pd.read_csv("notebooks/data/stud.csv")
+
             logging.info("Read the dataset as dataframe")
 
             os.makedirs(
@@ -37,8 +39,6 @@ class DataIngestion:
                 index=False,
                 header=True
             )
-
-            logging.info("Train test split initiated")
 
             train_set, test_set = train_test_split(
                 df,
@@ -58,11 +58,13 @@ class DataIngestion:
                 header=True
             )
 
-            logging.info("Ingestion of the data is completed")
+            logging.info(
+                "Ingestion of the data is completed"
+            )
 
             return (
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
             )
 
         except Exception as e:
@@ -70,5 +72,18 @@ class DataIngestion:
 
 
 if __name__ == "__main__":
+
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
+    train_data, test_data = (
+        obj.initiate_data_ingestion()
+    )
+
+    data_transformation = DataTransformation()
+
+    train_arr, test_arr, preprocessor_path = (
+        data_transformation.initiate_data_transformation(
+            train_data,
+            test_data
+        )
+    )
